211040a7141f87c4a487f9bf5746705b4515fde2
koenidv <32238636+koenidv@users.noreply.github.com>
2019-11-16 22:15:53 +0100

    Add randomized background image

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 window.onload = function() {
-    document.body.classList.remove("active");
+    document.body.classList.remove("active")
+
+    var bg = document.getElementById("background");
+    if (bg) {
+        bg.style.backgroundImage = "url(/files/bg_" + Math.round(this.Math.random() * 5) + ".jpg)"
+    }
+
 }
 
 $("a").not("[href*=com]").not("[href*=mosbacher-berg]").not("[href$=pdf]").click(function () {
-    document.body.classList.add("active");
+    document.body.classList.add("active")
 }) 
