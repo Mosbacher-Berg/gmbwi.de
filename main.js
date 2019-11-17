46034a952a22c07f41f08d34eb7733309bbfa2d7
koenidv <32238636+koenidv@users.noreply.github.com>
2019-11-17 11:29:39 +0100

    Fix load from bfcache

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 window.onload = function() {
     document.body.classList.remove("active")
 
-    var bg = document.getElementById("background");
+    var bg = document.getElementById("background")
     if (bg) {
         bg.style.backgroundImage = "url(/files/bg_" + Math.round(this.Math.random() * 5) + ".jpg)"
     }
+}
 
+window.onpageshow = function() {
+    document.body.classList.remove("active")
 }
 
 $("a").not("[href*=com]").not("[href*=mosbacher-berg]").not("[href$=pdf]").click(function () {
